faababd641e44ac2ef633aac3aa4fa0d7c9fbec0
vaishnavi <aakritivaishu@gmail.com>
2024-02-13 20:24:09 +0530

    aa

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+// import aa from "../assets/aa.jpg"
 const Dashboard = () => {
   const [items, setItems] = useState([]);
 
@@ -34,6 +34,8 @@ const Dashboard = () => {
   return (
     <div className="bg-gradient-to-br from-purple-400 to-indigo-500 min-h-screen">
       <div className="container mx-auto py-8 text-orange-950">
+       
+      {/* <img src={aa} alt="Aa" style={{ maxHeight: '100%', objectFit: 'cover' }} />  */}
         <h1 className="text-3xl font-bold text-orange-950 mb-4">Dashboard</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {items.map(item => (
